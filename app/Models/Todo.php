1eeb7530ba7cd8f370555cf4f9de6c963120aedb
Boonyakid Poompeng <boonyakid.poompeng@gmail.com>
2026-03-27 20:11:17 +0700

    feat: setup models with belongsTo and hasMany relationships

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'user_id'
+    ];
+
+    public function user ()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -29,4 +29,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    public function todos ()
+    {
+        return $this->hasMany(Todo::class);
+    }
 }
