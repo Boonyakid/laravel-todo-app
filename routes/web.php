dce11dc78594fa9632f650e39672fbfcb5215eeb
Boonyakid Poompeng <boonyakid.poompeng@gmail.com>
2026-03-27 20:50:05 +0700

    feat: setup web routes with auth and guest middleware

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->middleware('auth');
+
+Route::get('/register', function () {
+    return view('register');
+})->middleware('guest');
+Route::post('/register', [AuthController::class, 'registerPost']);
+
+Route::get('/login', function () {
+    return view('login');
+})->name('login')->middleware('guest');
+Route::post('/login', [AuthController::class, 'loginPost']);
+
+Route::post('/logout', [AuthController::class, 'logoutPost'])->middleware('auth');
